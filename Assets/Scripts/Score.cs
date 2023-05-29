6388f3c7632720dfc7886241390f0d5f30553553
Barış Gün <barisgunrfx@gmail.com>
2023-05-29 21:29:05 +0300

    Lesson 3
    GameManager, Score UI, Obstacle collisions added.

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    int score;
+    Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+    }
+
+    public void Scored()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
