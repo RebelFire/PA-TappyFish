b49f08a55300ce2d05f9733696f4689ca10b49f2
Barış Gün <barisgunrfx@gmail.com>
2023-05-31 21:36:22 +0300

    GameOver Panel Added
    GameOver Panel Added

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -7,7 +7,10 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     int score;
+    int highScore;
     Text scoreText;
+    public Text panelScore;
+    public Text panelHighScore;
 
     // Start is called before the first frame update
     void Start()
@@ -15,12 +18,24 @@ public class Score : MonoBehaviour
         score = 0;
         scoreText = GetComponent<Text>();
         scoreText.text = score.ToString();
+        panelScore.text = score.ToString();
+
+        highScore = PlayerPrefs.GetInt("highscore");
+        panelHighScore.text = highScore.ToString();
     }
 
     public void Scored()
     {
         score++;
         scoreText.text = score.ToString();
+        panelScore.text = score.ToString();
+
+        if(score > highScore)
+        {
+            highScore = score;
+            panelHighScore.text = highScore.ToString();
+            PlayerPrefs.SetInt("highscore", highScore);
+        }
     }
 
     // Update is called once per frame
